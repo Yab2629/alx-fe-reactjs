acae991bcda5ab66c66a6a79ff3aeed93f4d9b86
Yeabsira Zewdu <zzzyab1@gmail.com>
2025-08-01 23:11:23 +0300

    setup Github user search application

--- github-user-search/github-user-search/services/githubService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const GITHUB_API_URL = 'https://api.github.com';
-
-export const searchUser = async (username) => {
-  console.log(`Searching for user: ${username}`);
-  return { message: "Function not yet implemented." };
-};
